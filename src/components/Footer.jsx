4b261ae642a19dd3147ad5ee122c37cccad41679
Sreyaea <sreya.ea@gmail.com>
2026-08-21 14:13:45 +0530

    footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -30,7 +30,7 @@ export default function Footer() {
         </div>
 
         <div className="footer-bottom">
-          <p>&copy; 2024 Creatus Technologies. All rights reserved.</p>
+          <p>&copy; 2026 Creatus Technologies. All rights reserved.</p>
         </div>
       </div>
     </footer>
